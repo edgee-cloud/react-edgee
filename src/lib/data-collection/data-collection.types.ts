0da2a3d0e2edc6239467d65578da402204c54a0a
Jer Chassigneux <chassigneuxjb@gmail.com>
2025-02-07 10:01:59 +0100

    Export Types & Rework Type names (#11)

--- src/lib/data-collection/data-collection.types.ts
+++ src/lib/data-collection/data-collection.types.ts
@@ -1,6 +1,6 @@
 import { Edgee } from '../../index';
 
-export interface Page {
+export interface PageData {
   name?: string;
   category?: string;
   keywords?: string[];
@@ -12,22 +12,22 @@ export interface Page {
   properties?: Record<string, unknown>;
 }
 
-export interface User {
+export interface UserData {
   user_id?: string;
   anonymous_id?: string;
   edgee_id: string;
   properties?: Record<string, unknown>;
 }
 
-export interface Track {
+export interface TrackData {
   name?: string;
   properties?: Record<string, unknown>;
 }
 
-export type Consent = 'granted' | 'denied' | 'pending';
+export type ConsentStatus = 'granted' | 'denied' | 'pending';
 
 export type EdgeeMethod<T> = (arg: T, components?: Record<string, boolean>) => void;
-export type EdgeeConsentMethod = (arg: Consent) => void;
+export type EdgeeConsentMethod = (arg: ConsentStatus) => void;
 
 export type QueuedEvent<T> = {
   method: Exclude<keyof Edgee, 'consent'>;
@@ -36,5 +36,5 @@ export type QueuedEvent<T> = {
 
 export type QueuedConsentEvent = {
   method: 'consent';
-  args: [Consent];
+  args: [ConsentStatus];
 };
